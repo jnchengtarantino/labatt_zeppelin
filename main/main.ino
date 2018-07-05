8442da2215633415fb7cb80f6cf21c39b00f0450
ZacharyBys <zachary.bys@gmail.com>
2018-07-04 23:39:58 -0400

    Adjusted melody player for the guitar, currently playing star wars. Will adapt to synchronize lights on the board

--- main/main.ino
+++ main/main.ino
@@ -14,6 +14,43 @@
 #define ORANGE_BUT 12
 #define STRUM 13
 #define PUMP 11
+#define SPEAKER 9
+
+#define  c3    7634
+#define  d3    6803
+#define  e3    6061
+#define  f3    5714
+#define  g3    5102
+#define  a3    4545
+#define  b3    4049
+#define  c4    3816    // 261 Hz 
+#define  d4    3401    // 294 Hz 
+#define  e4    3030    // 329 Hz 
+#define  f4    2865    // 349 Hz 
+#define  g4    2551    // 392 Hz 
+#define  a4    2272    // 440 Hz 
+#define  a4s   2146
+#define  b4    2028    // 493 Hz 
+#define  c5    1912    // 523 Hz
+#define  d5    1706
+#define  d5s   1608
+#define  e5    1517    // 659 Hz
+#define  f5    1433    // 698 Hz
+#define  g5    1276
+#define  a5    1136
+#define  a5s   1073
+#define  b5    1012
+#define  c6    955
+ 
+#define  R     0      // Rest
+
+// Melody 1: Star Wars Theme
+int melody1[] = {  f4,  f4, f4,  a4s,   f5,  d5s,  d5,  c5, a5s, f5, d5s,  d5,  c5, a5s, f5, d5s, d5, d5s, c5};
+int beats1[]  = {  21,  21, 21,  128,  128,   21,  21,  21, 128, 64,  21,  21,  21, 128, 64,  21, 21,  21, 128}; 
+
+long tempo = 10000; // Set overall tempo
+int pause = 1000; // Set length of pause between notes
+int rest_count = 50; // Loop variable to increase Rest length (BLETCHEROUS HACK; See NOTES)
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
@@ -43,7 +80,9 @@ void setup() {
   pinMode(ORANGE_BUT, INPUT);
   pinMode(STRUM, INPUT);
   pinMode(PUMP, OUTPUT);
+  pinMode(SPEAKER, OUTPUT);
 
+  playMelody();
 }
 
 void loop() {
@@ -219,5 +258,41 @@ void show_orange()
       FastLED.show();
   
   }
+}
+
+void playTone(int toneM, int beat, long duration) {
+  long elapsed_time = 0;
+  if (toneM > 0) { // if this isn't a Rest beat, while the tone has 
+    //  played less long than 'duration', pulse speaker HIGH and LOW
+    while (elapsed_time < duration) {
+ 
+      digitalWrite(SPEAKER,HIGH);
+      delayMicroseconds(toneM / 2);
+ 
+      // DOWN
+      digitalWrite(SPEAKER, LOW);
+      delayMicroseconds(toneM / 2);
+ 
+      // Keep track of how long we pulsed
+      elapsed_time += (toneM);
+    } 
+  }
+  else { // Rest beat; loop times delay
+    for (int j = 0; j < rest_count; j++) { // See NOTE on rest_count
+      delayMicroseconds(duration);  
+    }                                
+  }                                 
+}
+
+void playMelody() {
+    for (int i=0; i<(sizeof(melody1)/sizeof(int)); i++) {
+      int toneM = melody1[i];
+      int beat = beats1[i];
+ 
+      long duration = beat * tempo; // Set up timing
+ 
+      playTone(toneM, beat, duration); // A pause between notes
+      delayMicroseconds(pause);
+    }
 }
 
